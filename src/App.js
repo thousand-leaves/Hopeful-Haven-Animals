a23493bfacf7ea5932de56f2a47446e26fdb2244
Adam Kay <adam.kay@fdmgroup.com>
2023-06-15 14:21:25 +0100

    updating with a settings page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
+import "./styles/SettingsPage.css";
 import Header from "./components/organisms/Header";
 import Home from "./components/pages/Home";
 import Reports from "./components/pages/Reports";
@@ -10,6 +11,7 @@ import LoginUser from "./components/pages/Login";
 import PageNotFound from "./components/pages/PageNotFound";
 import DonateForm from "./components/pages/DonationFormAttempt";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import SettingsPage from "./components/pages/SettingsPage";
 
 function App() {
   return (
@@ -20,10 +22,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="/donate" element={<Donate />} />
-          <Route path="/donateform" element={<DonateForm />} />
+          <Route path="/donateForm" element={<DonateForm />} />
           <Route path="/forums" element={<Forum />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<LoginUser />} />
+          <Route path="/settings" element={<SettingsPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
-  const [liveNews, setLiveNews] = useState([]);
+  const [liveNews, setLiveNews] = useState(null);
 
   useEffect(() => {
     fetchLiveNews();
@@ -18,42 +18,29 @@ const Home = () => {
   };
 
   const renderLiveNews = () => {
-    if (typeof liveNews !== "object" || Object.keys(liveNews).length === 0) {
+    if (!liveNews) {
       return <p>Loading LiveNews...</p>;
     }
 
-    const newsItems = Object.values(liveNews);
-    const liveNewsImages = Array.from({ length: 4 }, (_, index) => `/PicsForLN/pic${index + 1}.jpg`);
-
-    return newsItems.slice(0, 4).map((newsItem, index) => (
+    return liveNews.map((newsItem) => (
       <div className="card" key={newsItem.id}>
-        <img
-          src={liveNewsImages[index]}
-          alt="Live News"
-          className="card-img-left"
-        />
+        <img src={newsItem.image} alt="Live News" className="card-img-left" />
         <div className="card-body">
-          <h5 className="card-title">{newsItem.title || "Title Placeholder"}</h5>
-          <p className="card-text">{newsItem.description || "Description Placeholder"}</p>
+          <h5 className="card-title">{newsItem.title}</h5>
+          <p className="card-text">{newsItem.description}</p>
         </div>
       </div>
     ));
   };
 
   const renderRelatedNews = () => {
-    if (typeof liveNews !== "object" || Object.keys(liveNews).length === 0) {
+    if (!liveNews) {
       return <p>Loading Related News...</p>;
     }
 
-    const newsItems = Object.values(liveNews);
-
-    return newsItems.map((newsItem, index) => (
+    return liveNews.map((newsItem) => (
       <div className="card" key={newsItem.id}>
-        <img
-          src={`/PicsForLN/pic${index + 1}.jpg`} // Updated image source path
-          alt="Related News"
-          className="card-img-top"
-        />
+        <img src={newsItem.image} alt="Related News" className="card-img-top" />
         <div className="card-body">
           <h5 className="card-title">{newsItem.title}</h5>
         </div>
@@ -65,92 +52,36 @@ const Home = () => {
     <div>
       <div className="LiveNews">
         <h1 className="display">Live News</h1>
-        <p className="LNPlaceholderText">
-          <h3>HAPPENING NOW!</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
-        <p className="LNPlaceholderText">
-          <h3>BREAKING NEWS 1!</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
-        <p className="LNPlaceholderText">
-          <h3>BREAKING NEWS 2!</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
-        <p className="LNPlaceholderText">
-          <h3>BREAKING NEWS 3!</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
+        {renderLiveNews()}
       </div>
       
       <div className="RelatedNews">
         <h2>Related News</h2>
-        <p className="RNPlaceholderText">
-          <h3>Heading 1!</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
-        <p className="RNPlaceholderText">
-          <h3>Heading 2</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
-        <p className="RNPlaceholderText">
-          <h3>Heading 3!</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
-        <p className="RNPlaceholderText">
-          <h3>Heading 4!</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Perspiciatis impedit
-          est dignissimos eius ipsam asperiores possimus voluptate molestias
-          vero illo, fugiat quisquam excepturi ducimus?
-        </p>
+        {renderRelatedNews()}
       </div>
+
       <div className="Donations">
-        <h4>Recent Donations</h4>
-        <hr></hr>
-        <p className="DonationText">
-          <h3>User1479</h3>
+        <h2>Recent Donations</h2>
+        <p className="text-secondary">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Donated £347.00
-        </p>
-        <hr></hr>
-        <p className="DonationText">
-          <h3>User1479</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Donated £347.00
-        </p>
-        <hr></hr>
-        <p className="DonationText">
-          <h3>User1479</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Donated £347.00
-        </p>
-        <hr></hr>
-        <p className="DonationText">
-          <h3>User1479</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
-          tempora porro recusandae architecto perferendis. Donated £347.00
+          tempora porro recusandae architecto perferendis. Perspiciatis impedit
+          est dignissimos eius ipsam asperiores possimus voluptate molestias
+          vero illo, fugiat quisquam excepturi ducimus? Sequi iusto odit
+          laboriosam praesentium, incidunt eligendi tempora recusandae
+          explicabo, dolor voluptatem repellat harum obcaecati aspernatur cum.
+          Quod sint fuga, quidem perferendis culpa alias ipsum totam vitae.
+          Adipisci, enim quaerat. Officiis dolor distinctio repellendus suscipit
+          ipsum fuga, dignissimos reiciendis omnis tempore aut explicabo
+          temporibus minima cupiditate animi esse quis fugit officia rem modi
+          quas blanditiis eius. Aperiam veniam animi dolorem. Distinctio modi
+          magni, impedit blanditiis laborum amet nihil dolorum omnis porro a
+          dolores, earum accusamus recusandae tempore nemo! Consectetur adipisci
+          quam consequatur cupiditate in soluta expedita repudiandae tempore
+          amet consequuntur! Doloremque accusantium quibusdam, dignissimos
+          fugiat in iste dicta pariatur architecto quia ratione aperiam soluta
+          assumenda voluptate nesciunt nisi obcaecati et voluptatem natus
+          corporis. Adipisci non recusandae architecto eligendi tempora
+          similique!
         </p>
       </div>
     </div>
